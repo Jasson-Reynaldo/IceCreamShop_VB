22175d2f3fa59f84b734c1b9a2cc34ee7080c031
Jasson Reynaldo <58303071+JassonReynaldo@users.noreply.github.com>
2022-04-08 22:33:10 +0700

    Uploading The Finished Project File
    This Project is created originally by Jasson Reynaldo in November 2020

---
+++ IceCreamShopJr/OrderStatusForm.vb
@@ -0,0 +1,9 @@
+﻿Public Class OrderStatusForm
+
+    Dim selectedOrder As String
+    Private Sub btnComplete_Click(sender As Object, e As EventArgs) Handles btnComplete.Click
+        selectedOrder = chlboxOrder.SelectedItem
+        lsBoxDone.Items.Add(selectedOrder)
+        chlboxOrder.Items.Remove(selectedOrder)
+    End Sub
+End Class
